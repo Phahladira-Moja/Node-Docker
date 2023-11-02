77d59c05772009a0b0c5286fc945a92c2d99fbc1
Phahladira-Moja <phahladira@patala.co.za>
2023-11-02 20:49:40 +0200

    Added mongooses and gone through network section

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://root:root@mongo:27017/?authSource=admin")
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   return res.send("<h2>Hi there</h2>");
@@ -9,3 +15,4 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+// 172.29.0.2
